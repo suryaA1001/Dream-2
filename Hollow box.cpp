b2ea4bd0fc72e46b91ff604f6acd977fe73b456f
SURYA M <125336461+suryaA1001@users.noreply.github.com>
2024-10-16 12:03:28 +0530

    Add files via upload

---
+++ Hollow box.cpp
@@ -0,0 +1,20 @@
+import java.util.*;
+class main {
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+         int r = sc.nextInt();
+         int c = sc.nextInt();
+         for(int i=0;i<r;i++)
+         {
+             for(int j=0;j<c;j++)
+             {
+                 if(i==0 || i==r-1 || j==0 || j==c-1){
+                 System.out.print(" * ");}
+                 else{
+                     System.out.print("   ");
+                 }
+             }
+             System.out.println("");
+         }
+    }
+}
